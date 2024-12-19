d5d72d319fcdf4ac4c057c5340e7b288feef2658
maciej.koziara <maciej.koziara@playboycontractors.com>
2024-12-19 22:51:34 +0100

    [feat] Basic CLI commands

--- src/stub-server/cli.ts
+++ src/stub-server/cli.ts
@@ -1,7 +1,13 @@
 import { program } from "commander";
-import { mkdir } from "node:fs/promises";
+import { appendFile, mkdir } from "node:fs/promises";
 
 program.name("stub-server").description("CLI to manage test servers");
+
+const STUBS_DIR = "./stubs";
+const STUB_LOGS_DIR = `${STUBS_DIR}/logs`;
+const STUB_SERVERS_FILE = `${STUBS_DIR}/servers.txt`;
+const pathToLogFile = (instanceId: string) =>
+	`${STUB_LOGS_DIR}/${instanceId}.log`;
 
 program
 	.command("run")
@@ -13,123 +19,113 @@ program
 			options.port || Math.floor(Math.random() * (65535 - 3000) + 3000);
 		const instanceId = Math.random().toString(36).substr(2, 9);
 
-		await mkdir("./stubs/logs", { recursive: true })
-		const logFile = Bun.file(`./stubs/logs/${instanceId}.log`)
+		await mkdir(STUBS_DIR, { recursive: true });
+		const logFile = Bun.file(pathToLogFile(instanceId));
 
+		const proc = Bun.spawn(
+			["bun", `--port=${port}`, "./src/stub-server/server.ts"],
+			{
+				stdout: logFile,
+				env: {
+					...process.env,
+					SERVER_NUMBER: instanceId,
+				},
+			},
+		);
 
-		const proc = Bun.spawn(["bun", `--port=${port}`, "./src/stub-server/server.ts"], {
-			stdout: logFile,
-			env: {
-				...process.env,
-				SERVER_NUMBER: instanceId
-			}
-		})
+		await appendFile(STUB_SERVERS_FILE, `${instanceId}|${proc.pid}|${port}\n`);
 
 		if (options.detached) {
-			proc.unref()
+			proc.unref();
 		}
-		proc.ref()
-
-
-		// await new Promise((resolve, reject) => {
-		// 	pm2.connect((err) => {
-		// 		if (err) {
-		// 			reject(err);
-		// 			return;
-		// 		}
-		//
-		// 		pm2.start(
-		// 			{
-		// 				script: "src/stub-server/index.ts",
-		// 				name: `${PM2_APP_NAME}-${instanceId}`,
-		// 				interpreter: "bun",
-		// 				env: {
-		// 					PORT: port.toString(),
-		// 					SERVER_NUMBER: instanceId,
-		// 					PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
-		// 				},
-		// 			},
-		// 			(err) => {
-		// 				if (err) reject(err);
-		// 				else {
-		// 					console.log(
-		// 						`Server started with ID: ${instanceId} on port: ${port}`,
-		// 					);
-		// 					resolve(null);
-		// 				}
-		// 				pm2.disconnect();
-		// 			},
-		// 		);
-		// 	});
-		// });
 	});
 
-// program
-// 	.command("list")
-// 	.description("List all running servers")
-// 	.action(async () => {
-// 		await new Promise((resolve) => {
-// 			pm2.connect(() => {
-// 				pm2.list((err, list) => {
-// 					if (err) {
-// 						console.error("Error listing servers:", err);
-// 					} else {
-// 						const servers = list.filter((p) =>
-// 							p.name?.startsWith(PM2_APP_NAME),
-// 						);
-// 						servers.forEach((p) => {
-// 							console.log(
-// 								`ID: ${p.name?.split("-")[2]}, PID: ${p.pid}, Status: ${p.pm2_env?.status}`,
-// 							);
-// 						});
-// 					}
-// 					pm2.disconnect();
-// 					resolve(null);
-// 				});
-// 			});
-// 		});
-// 	});
+const loadRunningServers = async () => {
+	return (await Bun.file(STUB_SERVERS_FILE).text())
+		.split("\n")
+		.filter(s => s.length > 0)
+		.map((s) => s.split("|"))
+		.map(([instanceId, pid, port]) => ({ instanceId, pid, port }));
+};
 
-// program
-// 	.command('logs')
-// 	.argument('<id>', 'Server ID')
-// 	.description('Show logs for a specific server')
-// 	.action(async (id) => {
-// 		await new Promise((resolve) => {
-// 			pm2.connect(() => {
-// 				pm2.describe(`${PM2_APP_NAME}-${id}`, (err, proc) => {
-// 					if (err || !proc.length) {
-// 						console.error(`Server ${id} not found`);
-// 					} else {
-// 						pm2.logs(`${PM2_APP_NAME}-${id}`, {
-// 							lines: 1000,
-// 							timestamp: true
-// 						});
-// 					}
-// 					resolve(null);
-// 				});
-// 			});
-// 		});
-// 	});
+const isRunning = (
+	server: Awaited<ReturnType<typeof loadRunningServers>>[number],
+) => {
+	try {
+		process.kill(Number(server.pid), 0);
+		return true;
+	} catch (e) {
+		return false;
+	}
+};
 
-// program
-// 	.command("stop")
-// 	.argument("<id>", "Server ID")
-// 	.description("Stop a specific server")
-// 	.action(async (id) => {
-// 		await new Promise((resolve) => {
-// 			pm2.connect(() => {
-// 				pm2.delete(`${PM2_APP_NAME}-${id}`, (err) => {
-// 					if (err) {
-// 						console.error(`Error stopping server ${id}:`, err);
-// 					} else {
-// 						console.log(`Server ${id} stopped`);
-// 					}
-// 					pm2.disconnect();
-// 					resolve(null);
-// 				});
-// 			});
-// 		});
-// 	});
+const revalidateProcesses = (
+	runningServers: Awaited<ReturnType<typeof loadRunningServers>>,
+) => {
+	const actuallyRunning = runningServers.filter(isRunning);
+	const content = actuallyRunning
+		.reduce(
+			(s1, { instanceId, pid, port }) => s1 + `${instanceId}|${pid}|${port}\n`,
+			"",
+		);
+
+	const writer = Bun.file(STUB_SERVERS_FILE).writer();
+	writer.write(content);
+	writer.flush();
+
+	return actuallyRunning
+};
+
+program
+	.command("list")
+	.description("List all running servers")
+	.action(async () => {
+		const runningServers = await loadRunningServers();
+		const actuallyRunning = revalidateProcesses(runningServers)
+
+		actuallyRunning.forEach(({ port, pid, instanceId }, index) => {
+			console.log(`Server ${index + 1}: ${instanceId} - ${pid} - :${port}`);
+		});
+	});
+
+program
+	.command('logs')
+	.argument('<id>', 'Server ID')
+	.description('Show logs for a specific server')
+	.action(async (id) => {
+		const runningServers = await loadRunningServers();
+		const actuallyRunning = revalidateProcesses(runningServers)
+
+		const wantedServer = actuallyRunning.find(({instanceId}) => instanceId === id)
+		if (!wantedServer) {
+			console.log("Server not found")
+			return
+		}
+
+		console.log(await Bun.file(pathToLogFile(wantedServer.instanceId)).text())
+
+	});
+
+program
+	.command("stop")
+	.argument("<id>", "Server ID")
+	.description("Stop a specific server")
+	.action(async (id) => {
+		const runningServers = await loadRunningServers();
+		const actuallyRunning = revalidateProcesses(runningServers)
+
+		const serverToKill = actuallyRunning.find(({instanceId}) => instanceId === id)
+		if (!serverToKill) {
+			console.log(`Server ${id} is not running`)
+			return
+		}
+
+		if (!isRunning(serverToKill)) {
+			console.log(`Server ${id} is not running`)
+			return
+		}
+
+		process.kill(Number(serverToKill.pid))
+	});
 
 program.parse();
