c756aeff5ac6cc4f098e534bb6449bf0fa552698
pacific112 <maciej.koziara@yahoo.pl>
2025-01-29 15:05:33 +0100

    [chore] Add Basic support for path params

--- src/load-balancer/router.ts
+++ src/load-balancer/router.ts
@@ -2,6 +2,7 @@ import z from "zod";
 
 type Params = {
 	req: Request;
+	pathParams: Record<string, string>;
 };
 type Handler = (p: Params) => Response;
 type HandlerWithBody<T> = (body: T, p: Params) => Response | Promise<Response>;
@@ -16,10 +17,16 @@ type PostRoute = {
 	bodySchema: z.ZodTypeAny;
 	handler: HandlerWithBody<unknown>;
 };
-type RouteDefinition = GetRoute | PostRoute;
+type DeleteRoute = {
+	method: "DELETE";
+	path: string;
+	handler: Handler;
+};
+type RouteDefinition = GetRoute | PostRoute | DeleteRoute;
 
 type TrieNode = {
 	path: string;
+	param: boolean;
 	route?: RouteDefinition;
 	nodes: Map<string, TrieNode>;
 };
@@ -47,6 +54,17 @@ export const post = <T extends z.ZodTypeAny>(
 	handler,
 });
 
+export const del = <T extends string>(
+	path: T,
+	handler: Handler,
+): DeleteRoute => {
+	return {
+		method: "DELETE",
+		path,
+		handler,
+	};
+};
+
 const splitPath = (path: string) => path.split("/").filter((p) => p !== "");
 
 const buildTrieRouter = (routes: RouteDefinition[]) => {
@@ -59,7 +77,11 @@ const buildTrieRouter = (routes: RouteDefinition[]) => {
 		let currentNode: TrieRoot | TrieNode = trie.get(route.method)!;
 		for (const path of splitPath(route.path)) {
 			if (!currentNode.nodes.has(path)) {
-				currentNode.nodes.set(path, { path, nodes: new Map() });
+				currentNode.nodes.set(path, {
+					path,
+					nodes: new Map(),
+					param: path.startsWith(":"),
+				});
 			}
 			currentNode = currentNode.nodes.get(path)!;
 		}
@@ -82,32 +104,56 @@ const findRequestHandler = (
 	const pathParts = splitPath(path);
 
 	let currentNode: TrieRoot | TrieNode | undefined = trieRouter.get(method);
+	let params: { name: string; position: number }[] = [];
 	for (const pathPart of pathParts) {
 		if (!currentNode) break;
-		currentNode = currentNode.nodes.get(pathPart);
+		currentNode =
+			currentNode.nodes.get(pathPart) ??
+			currentNode.nodes.values().find((d) => d.param);
+		if (currentNode?.param) {
+			params.push({
+				name: currentNode.path,
+				position: pathParts.indexOf(pathPart),
+			});
+		}
 	}
 
-	return currentNode;
+	return [currentNode, params] as const;
+};
+
+const extractParamsFromPath = (
+	req: Request,
+	params: { name: string; position: number }[],
+) => {
+	const pathParts = splitPath(new URL(req.url).pathname);
+	return params.reduce(
+		(prev, curr) => ({
+			...prev,
+			[curr.name.slice(1)]: pathParts[curr.position],
+		}),
+		{},
+	);
 };
 
 export const router = (...routes: RouteDefinition[]) => {
 	const trieRouter = buildTrieRouter(routes);
 
 	return async (req: Request) => {
-		const reqHandler = findRequestHandler(trieRouter, req);
+		const [reqHandler, params] = findRequestHandler(trieRouter, req);
 		if (!reqHandler || !reqHandler.route) {
 			return new Response(undefined, { status: 404 });
 		}
 
+		const pathParams = extractParamsFromPath(req, params);
 		const { route } = reqHandler;
 		if (!("bodySchema" in route)) {
-			return route.handler({ req });
+			return route.handler({ req, pathParams });
 		}
 
-		const jsonBody = await req.json()
+		const jsonBody = await req.json();
 		const parsedBody = await route.bodySchema.safeParseAsync(jsonBody);
 		if (parsedBody.success) {
-			return route.handler(parsedBody.data, { req });
+			return route.handler(parsedBody.data, { req, pathParams });
 		}
 
 		return new Response(`Cannot parse config: ${parsedBody.error.message}`, {
