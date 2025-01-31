e5e814ba3646daed8ba39016d19019c2d1188fba
pacific112 <maciej.koziara@yahoo.pl>
2025-01-31 14:24:37 +0100

    [feat] Introduce control panel for node

--- src/dashboard/src/components/server-flow.tsx
+++ src/dashboard/src/components/server-flow.tsx
@@ -11,6 +11,10 @@ import "@xyflow/react/dist/style.css";
 import { Server } from "@/types/types.ts";
 import { useMemo } from "react";
 import { cn } from "@/lib/utils.ts";
+import { Popover, PopoverContent } from "@/components/ui/popover.tsx";
+import { PopoverTrigger } from "@radix-ui/react-popover";
+import { DeleteServerDialog } from "@/components/delete-server-dialog.tsx";
+import { ShowLogsDialog } from "@/components/show-logs-dialog.tsx";
 
 const LoadBalancerNode = ({ data }: NodeProps) => (
 	<div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400">
@@ -28,31 +32,41 @@ const LoadBalancerNode = ({ data }: NodeProps) => (
 	</div>
 );
 
-const ServerNode = ({ data }: NodeProps) => (
-	<div
-		className={cn(
-			"px-4 py-2 shadow-md rounded-md bg-white border-2 border-blue-400",
-			{
-				"border-blue-400": data.status === "online",
-				"border-red-400": data.status === "offline",
-				"border-gray-400": data.status === "loading",
-			},
-		)}
-	>
-		{data.status === "online" ? (
-			<Handle
-				type="target"
-				position={Position.Top}
-				className="w-16 !bg-blue-400"
-			/>
-		) : null}
-		<div className="flex items-center">
-			<div className="ml-2">
-				<div className="text-lg font-bold">{data.label}</div>
-				<div className="text-gray-500">Handles Requests</div>
+const ServerNode = ({ data: { server } }: NodeProps) => (
+	<Popover>
+		<PopoverTrigger asChild>
+			<div
+				className={cn(
+					"px-4 py-2 shadow-md rounded-md bg-white border-2 border-blue-400",
+					{
+						"border-blue-400": server.status === "online",
+						"border-red-400": server.status === "offline",
+						"border-gray-400": server.status === "loading",
+					},
+				)}
+			>
+				{server.status === "online" ? (
+					<Handle
+						type="target"
+						position={Position.Top}
+						className="w-16 !bg-blue-400"
+					/>
+				) : null}
+				<div className="flex items-center">
+					<div className="ml-2">
+						<div className="text-lg font-bold">{server.id}</div>
+						<div className="text-gray-500">Handles Requests</div>
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
+		</PopoverTrigger>
+		<PopoverContent className="w-fit p-1">
+			<div className="flex">
+				{server.status === "online" && <DeleteServerDialog server={server} />}
+				<ShowLogsDialog server={server} />
+			</div>
+		</PopoverContent>
+	</Popover>
 );
 
 const loadBalancerNode: Node = {
@@ -77,7 +91,7 @@ export const ServerFlow = ({ servers }: Props) => {
 			...servers.map((s, i) => ({
 				id: s.id,
 				type: "server",
-				data: { label: s.id, status: s.status },
+				data: { server: s },
 				position: {
 					x: 250 * (i % 5),
 					y: 200 + 100 * Math.floor(i / 5),
