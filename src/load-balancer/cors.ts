18313a873c347cfc40e2fd179d613ec887448f20
pacific112 <maciej.koziara@yahoo.pl>
2025-02-11 22:20:49 +0100

    [chore] Improve cors

--- src/load-balancer/cors.ts
+++ src/load-balancer/cors.ts
@@ -2,18 +2,25 @@ type RequestHandler = (req: Request) => Promise<Response>;
 
 const applyCorsHeaders = (res: Response) => {
 	res.headers.set("Access-Control-Allow-Origin", "*");
+	res.headers.set("Access-Control-Max-Age", "1800");
+	res.headers.set("Access-Control-Allow-Headers", "Content-Type");
 	res.headers.set(
 		"Access-Control-Allow-Methods",
 		"GET, POST, PUT, DELETE, OPTIONS",
 	);
 
-	return res
-}
+	return res;
+};
 
 export const cors = (handle: RequestHandler): RequestHandler => {
 	return async (req) => {
 		if (req.method === "OPTIONS") {
-			return applyCorsHeaders(new Response)
+			return applyCorsHeaders(
+				new Response(null, {
+					status: 204,
+					statusText: "No Content",
+				}),
+			);
 		}
 
 		const res = await handle(req);
