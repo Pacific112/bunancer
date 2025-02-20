31f009574e096d17abaec0c0ba7c324977f5190e
pacific112 <maciej.koziara@yahoo.pl>
2025-02-20 22:30:10 +0200

    [chore] Make MAIN_DIR more generic, improve caching

--- packages/load-balancer/src/routing/public-folder.ts
+++ packages/load-balancer/src/routing/public-folder.ts
@@ -1,22 +1,22 @@
 import { get } from "@/routing/router.ts";
 import type { BunFile } from "bun";
 
+const MAIN_DIR = Bun.main.replace(/\/[^/]+$/, "");
+
 const cache = async (res: Response, file: BunFile) => {
+	const arrayBuffer = await file.arrayBuffer();
+	const hash = Bun.hash(arrayBuffer);
+
 	res.headers.set("Cache-Control", "public, max-age=31536000, immutable");
-	res.headers.set(
-		"Expires",
-		new Date(Date.now() + 31536000 * 1000).toUTCString(),
-	);
-	res.headers.set("ETag", `W/"${Bun.hash(await file.arrayBuffer())}"`);
+	res.headers.set("ETag", `"${hash}"`);
 	res.headers.set("Last-Modified", new Date(file.lastModified).toUTCString());
 
 	return res;
 };
 
-export const publicFolder = (folder = "/public") =>
+export const publicFolder = (folder: `/${string}` = "/public") =>
 	get(`${folder}/:fileName`, async ({ pathParams: { fileName } }) => {
-		const rootDir = Bun.main.replace("/index.tsx", "");
-		const file = Bun.file(`${rootDir}/public/${fileName}`);
+		const file = Bun.file(`${MAIN_DIR}${folder}/${fileName}`);
 		if (await file.exists()) {
 			return cache(new Response(file), file);
 		}
