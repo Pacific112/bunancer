602ce78b65c68253d4a1d16e2170e50962cbd09d
pacific112 <maciej.koziara@yahoo.pl>
2025-02-25 14:11:12 +0100

    [chore] Restore dark style

--- packages/load-balancer/ui/components/ui/button.tsx
+++ packages/load-balancer/ui/components/ui/button.tsx
@@ -5,20 +5,20 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "$/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 redark:focus-visible:ring-zinc-300",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-zinc-300",
   {
     variants: {
       variant: {
         default:
-          "bg-zinc-900 text-zinc-50 shadow-sm hover:bg-zinc-900/90 redark:bg-zinc-50 redark:text-zinc-900 redark:hover:bg-zinc-50/90",
+          "bg-zinc-900 text-zinc-50 shadow-sm hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
         destructive:
-          "bg-red-500 text-zinc-50 shadow-xs hover:bg-red-500/90 redark:bg-red-900 redark:text-zinc-50 redark:hover:bg-red-900/90",
+          "bg-red-500 text-zinc-50 shadow-xs hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90",
         outline:
-          "border border-zinc-200 bg-white shadow-xs hover:bg-zinc-100 hover:text-zinc-900 redark:border-zinc-800 redark:bg-zinc-950 redark:hover:bg-zinc-800 redark:hover:text-zinc-50",
+          "border border-zinc-200 bg-white shadow-xs hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
         secondary:
-          "bg-zinc-100 text-zinc-900 shadow-xs hover:bg-zinc-100/80 redark:bg-zinc-800 redark:text-zinc-50 redark:hover:bg-zinc-800/80",
-        ghost: "hover:bg-zinc-100 hover:text-zinc-900 redark:hover:bg-zinc-800 redark:hover:text-zinc-50",
-        link: "text-zinc-900 underline-offset-4 hover:underline redark:text-zinc-50",
+          "bg-zinc-100 text-zinc-900 shadow-xs hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
+        ghost: "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
+        link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
       },
       size: {
         default: "h-9 px-4 py-2",
